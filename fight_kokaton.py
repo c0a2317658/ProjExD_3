10ed125e71b6f05c004badd0c7dd8ac35a1f8314
C0A23176 <c0a2317658@edu.teu.ac.jp>
2024-06-25 16:00:09 +0900

    練習４爆弾複数

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -7,6 +7,7 @@ import pygame as pg
 
 WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
+NUM_OF_BOMBS = 5
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -146,7 +147,7 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
     beam = None
-    bomb = Bomb((255, 0, 0), 10)
+    bomb = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -165,13 +166,15 @@ def main():
                 pg.display.update()
                 time.sleep(1)
                 return
-        if bomb is not None:
-            if beam is not None:
-                if bomb.rct.colliderect(beam.rct):
-                    bomb = None
-                    beam = None
-                    bird.change_img(6, screen)
-                    
+
+        fro i in range(len(bombs)):
+                if beam is not None:
+                    if bomb[i].rct.colliderect(beam.rct):
+                        bomb[i] = None
+                        beam = None
+                        bird.change_img(6, screen)
+        bombs = [bomb for bomb in bombs i not None]
+
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
